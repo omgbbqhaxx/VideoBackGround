f8fb72763a6ba9b7c2737ac5a786b9ef7e81dc85
Homeros Bilişim <homerosbilisim@homeros-mac-mini.local>
2016-07-01 18:32:57 +0300

    Initial Commit

---
+++ VideoBackGround/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoBackGround
+//
+//  Created by Homeros Bilişim on 1.07.2016.
+//  Copyright © 2016 Homeros Bilişim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
